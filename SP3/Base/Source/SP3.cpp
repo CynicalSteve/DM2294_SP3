04d447b6ed5849957f7f02aff098be0a779f4590
homeworkace <rickchongrockching@gmail.com>
2017-08-29 16:47:19 +0800

    Synchronisation commit.

--- SP3/Base/Source/SP3.cpp
+++ SP3/Base/Source/SP3.cpp
@@ -37,14 +37,14 @@ void SP3::Init()
 		++mapSize;
 	alienBase::createSpawnPosition(theMap, mapSize);
 
-	/*for (short y = mapSize - 1; y > -1; --y)
+	for (short y = mapSize - 1; y > -1; --y)
 	{
 		for (short x = 0; x < mapSize; ++x)
 			std::cout << theMap[x][y] << " ";
 		std::cout << "\n";
 	}
 	for (short i = 0; i < alienBase::spawnPosition.size(); ++i)
-	std::cout << alienBase::spawnPosition[i].x << " " << alienBase::spawnPosition[i].y << "\n";*/
+	std::cout << alienBase::spawnPosition[i].x << " " << alienBase::spawnPosition[i].y << "\n";
 
 	pauseSelection = CONTINUE;
 	pauseSelectionIterator = 0;
